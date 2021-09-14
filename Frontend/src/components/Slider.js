1800f7aa00cbe383dc23a219aa03fd7a224fb184
Lamia Gülsah <guelsah-lamia@gmx.de>
2021-09-15 00:07:54 +0200

    backend vorbereitet

---
+++ Frontend/src/components/Slider.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Slider() {
+    return (
+        <div>
+            <p>Slider</p>
+        </div>
+    )
+}
+
+export default Slider
---
+++ Frontend/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Nav from "../components/Nav.js";
+import Slider from "../components/Slider.js";
+import Footer from "../components/Footer.js";
+
+
+function Home() {
+    return (
+        <div>
+            <Nav/>
+            <Slider/>
+            <h1>Home</h1>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Home
