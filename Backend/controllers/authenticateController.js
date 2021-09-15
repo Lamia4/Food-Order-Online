e4c3243f5ae5cc762dd2c29e212c02d530fc9d82
Lamia Gülsah <guelsah-lamia@gmx.de>
2021-09-15 14:11:59 +0200

    authenticate, login, logout

---
+++ Backend/controllers/authenticateController.js
@@ -0,0 +1,29 @@
+import User from "../models/User.js";
+import getToken from "../lib/token.js";
+
+export default {
+    loginByEmail: async function (req, res, next) {
+        try {
+            const { name, email, password } = req.body;
+            const authenticatedUser = await User.authenticate(email, password);
+            if(authenticatedUser){
+                const token = await getToken.createToken(name, email);
+                res.cookie("token", token, {
+                    maxAge: 60 * 1000,
+                    httpOnly: true,
+                })
+                res.send({token})
+            }
+            else {
+                res.send("authentication failed")
+            }
+        } catch (error) {
+            next(error);
+        }
+    },
+    logout : async function (req, res) {
+        res.clearCookie("token");
+        res.send("du bist ausgeloggt");
+    }
+
+}
