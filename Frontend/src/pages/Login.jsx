a08c28dd07d671938a72916ede91381fbabd1633
ahmadaflak8 <ahmadaflak8@gmail.com>
2021-09-16 09:07:32 +0200

    login

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
+import "./Login.css"
 
-function Login() {
-    return (
-        <div>
-            Login
-        </div>
-    )
+
+function Login()
+{
+    return(
+      
+        
+<div class="contact-section">
+
+<h1>Login</h1>
+{/* <div class="border"></div> */}
+
+<form class="contact-form" action="index.html" method="post">
+  <input type="text" class="contact-form-text" placeholder="User Name or Email"/>
+  <input type="password" class="contact-form-text" placeholder="Your password"/>
+  <input type="submit" class="contact-form-btn" value="Send"/>
+</form>
+</div>
+
+    );
 }
-
-export default Login
+export default Login;
