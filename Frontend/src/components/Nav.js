ca66f394f55fd5816b589bcf5c819481ea0fce2d
ZeynepGultekin <zeynep.pehlivan@windowslive.com>
2021-09-18 01:03:17 +0200

    pizza.js removed from nav.js

--- Frontend/src/components/Nav.js
+++ Frontend/src/components/Nav.js
@@ -1,50 +1,42 @@
-import React, { useState } from 'react';
+import React from 'react';
 import logo from "../img/logo.png"
 import login from "../img/login.png"
-import shopping from "../img/shopping.png"
 import "./Nav.css";
-import { NavLink } from 'react-router-dom';
-import { Container, Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem } from "reactstrap";
-
+import { BrowserRouter,NavLink, Route, Switch } from 'react-router-dom';
 
 import Slider from "../pages/Slider.jsx"
 import About from "../pages/About.jsx"
-import Categories from "../components/Categories.js";
-import Contact from "../pages/Contact.jsx";
-import Login from "../pages/Login.jsx";
+import Categories from "../components/Categories.js"
+import Contact from "../pages/Contact.jsx"
+import Login from "../pages/Login.jsx"
 
 
-
-
-function NavBar() {
-    const [isOpen, setIsOpen] = useState(false);
-    const toggle = () => setIsOpen(!isOpen)
+function Nav() {
     return (
 
-        
-            <div className="navbar">
-            <Container fluid className="p-2" style={{justifyItems: "stretch"}}>
-            <Navbar color="danger" dark expand="md" fixed="top" className="mr-2">
-                <NavbarBrand className="logo"><img src= {logo} alt="logo"/></NavbarBrand>
-                <NavbarToggler onClick={toggle}/>
-                <Collapse isOpen={isOpen} navbar>
-                    <Nav navbar>
-                        <NavItem >
-                            <NavLink to="/" className="navItems">Home</NavLink>
-                            <NavLink to="/categories" className="navItems">Categories</NavLink>
-                            <NavLink to="/contact" className="navItems">Contact</NavLink>
-                            <NavLink to="/about" className="navItems">About</NavLink>
-                        </NavItem>
-                    </Nav>
-                </Collapse>
-                <NavItem>
-                <NavLink to="/shopping" ClassName="shopping" ><img style={{height: "5vh"}} src= {shopping} alt="shopping"/></NavLink> 
-                <NavLink to="/shopping" ClassName="loginIcon" ><img style={{height: "5vh"}} src= {login} alt="login"/></NavLink> 
-                </NavItem>
-                </Navbar>
-            </Container>    
-            </div>
+        <BrowserRouter>
+            <div className="nav">
+                <div id="logo">
+                <a to="/" exact activeClassName="active">
+                <img src= {logo} alt="First slide"/>
+                </a>
+                <h1>  <NavLink to="/" exact activeClassName="active">PIZZA</NavLink></h1>
+                    
+                </div>
 
+                <ul className="menu">
+                    <li><NavLink to="/" exact activeClassName="active">Home</NavLink></li>
+                    <li><NavLink to="/categories" exact activeClassName="active">Categories</NavLink></li>
+                    <li><NavLink to="/contact" exact activeClassName="active">Contact</NavLink></li>
+                    <li><NavLink to="/about" exact activeClassName="active">About</NavLink></li>
+                </ul>
+
+                <div id="login">
+                <a href="/login"><img src= {login} alt="First slide"/>
+                </a>
+                </div>
+
+            </div>
 
             <Switch>
             <Route exact path="/" component={ Slider } />
@@ -53,17 +45,15 @@ function NavBar() {
             <Route path="/about" component={ About } />
             <Route path="/login" component={ Login } />
             
-          
-
             
+            </Switch>
 
-            
-
-    
+      
+            </BrowserRouter>
 
             
        
     )
 }
 
-export default NavBar
+export default Nav
