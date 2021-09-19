5efca7514497bbca31bbce38063bf53fc67963be
ZeynepGultekin <zeynep.pehlivan@windowslive.com>
2021-09-19 16:54:22 +0200

    Revert "categories finished"
    This reverts commit 0b095fe69366fdfcddd769d1db862dd6b414617a.

--- Frontend/src/components/Categories.js
+++ Frontend/src/components/Categories.js
@@ -2,25 +2,40 @@ import React from 'react'
 import { Container, Card, CardTitle, Button, CardImg,Row,Col,CardBody } from 'reactstrap';
 import {Link} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Dessert1 from "../img/dessert1.jpg";
+import Dessert2 from "../img/dessert2.jpg";
 import Hamburger from "../img/hamburger.jpg";
 import Pizza2 from "../img/pizza2.jpg";
 import Cookie from "../img/cookie.jpg";
-import "./Categories.css";
+import Kebab from "../img/kebab.jpg";
+import "./Categories.css"
 
 function Categories() {
     return (
         <div>
             <Container >
                 <Row className="smCenter justify-content-md-space-between productRow mt-3">
+                    <Col  xs ={10} md={6} lg={4} style={{height:"55vh"}} className=" mb-2">
+                        <Card className="d-flex " style={{color:"black", height:"100%"}}  inverse >
+                               <CardImg src={Dessert1} style={{height
+                            :"100%"}}/>
+                                <CardBody className="cardBody">
+                                    <CardTitle className="mb-3">
+                                        <Link to="/dessert"> <h1>Dessert</h1></Link>
+                                    </CardTitle>
+                                    <Button className="categoryButton">Order Now</Button>
+                                </CardBody>
+                        </Card>
+                    </Col>
                     <Col xs ={10} md={6} lg={4} style={{height:"55vh"}} className=" mb-2">
                         <Card className="d-flex" style={{color:"black", height:"100%"}}  inverse >
                                <CardImg src={Hamburger} style={{height
                             :"100%"}}/>
                                 <CardBody className="cardBody">
                                     <CardTitle>
-                                        <h1>Hamburger</h1>
+                                       <Link to="/hamburger"> <h1>Hamburger</h1></Link>
                                     </CardTitle>
-                                    <Link to="/hamburger"><Button className="categoryButton">See more</Button></Link>
+                                    <Button className="categoryButton">Order Now</Button>
                                 </CardBody>
                         </Card>
                     </Col>
@@ -30,9 +45,9 @@ function Categories() {
                             :"100%"}}/> 
                                <CardBody className="cardBody">
                                     <CardTitle>
-                                        <h1>Pizza</h1> 
+                                       <Link to="/pizza"> <h1>Pizza</h1> </Link>
                                     </CardTitle>
-                                    <Link to="/pizza" ><Button className="categoryButton">See more</Button></Link>
+                                    <Button className="categoryButton">Order Now</Button>
                                 </CardBody>
                         </Card>
                     </Col>
@@ -42,9 +57,21 @@ function Categories() {
                             :"100%"}}/>
                                 <CardBody className="cardBody">
                                     <CardTitle className="mb-3">
-                                        <h1>Dessert</h1> 
+                                       <Link to="/dessert"> <h1>Dessert</h1>  </Link>
                                     </CardTitle>
-                                    <Link to="/dessert"><Button className="categoryButton">See more</Button></Link>
+                                    <Button className="categoryButton">Order Now</Button>
+                                </CardBody>
+                        </Card>
+                    </Col>
+                    <Col xs ={10} md={6} lg={4} style={{height:"55vh"}} className=" mb-2">
+                        <Card className="d-flex categoryCard" style={{color:"black", height:"100%"}}  inverse >
+                               <CardImg src={Kebab} style={{height
+                            :"100%"}}/>
+                                <CardBody className="cardBody">
+                                    <CardTitle>
+                                       <Link to="/kebab"> <h1>Kebab</h1></Link>
+                                    </CardTitle>
+                                    <Button className="categoryButton">Order Now</Button>
                                 </CardBody>
                         </Card>
                     </Col>
