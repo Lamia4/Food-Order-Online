c02a321c2e91705e0e73c54eacff789bcda99af3
ZeynepGultekin <zeynep.pehlivan@windowslive.com>
2021-09-24 10:24:55 +0200

    lamia

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,7 +4,7 @@ export default {
 
     getProducts: async function (req, res, next) {
         try {
-            const products = await Product.find()
+            const products = await Product.find().populate("category")
             res.json(products)
         } catch (err) {
             return res.status(500).json({msg: err.message})
@@ -12,12 +12,9 @@ export default {
     },
     createProduct: async function (req, res, next) {
         try {
-            const {product_id, title, price, description, image, category} = req.body;
+            const {title, price, description, image, category} = req.body;
             if(!image) return res.status(400).json({msg: "pls add an image"});
-            const product = await Product.findOne({product_id});
-            if(product) return res.status(400).json({msg: "this product already exists"})
             const newProduct = new Product({
-                product_id,
                 title: title.toLowerCase(),
                 price,
                 description,
@@ -32,14 +29,20 @@ export default {
     },
     deleteProduct: async function (req, res, next) {
         try {
-            
+            await Product.findByIdAndDelete(req.params.id)
+            res.json({msg: "deleted a product"})
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
     },
     updateProduct: async function (req, res, next) {
         try {
+            const {title, price, description, image, category} = req.body;
             
+            await Product.findByIdAndUpdate(req.params.id, {
+                title: title.toLowerCase(), price, description, image, category
+            })
+            res.json({msg: "updated a product"})
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
