f5c77bd2f9e911ce3362f9c00b58671092bb0ed7
Lamia Gülsah <guelsah-lamia@gmx.de>
2021-10-24 22:18:42 +0200

    final before merge

--- Frontend/src/components/Logout.js
+++ Frontend/src/components/Logout.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import { CardText } from "reactstrap";
+import { CardText, Button } from "reactstrap";
+import { useHistory }from "react-router-dom";
 
 function Logout() {
+
+    const history= useHistory();
+    const handleHomepage = () => {
+        history.push("/")
+    };
 
     return (
         <div style={{textAlign:'center', marginTop: "7vh", fontSize: "25px"}}>
             <CardText style={{color: "white"}}>You are logged out!</CardText>
+            <Button style={{marginRight:"20px", backgroundColor:"#A61C3C", border:"none", borderRadius:"10px"}} onClick={handleHomepage}>Homepage</Button>
         </div>
     )
 }
